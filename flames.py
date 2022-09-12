c19c360e0d57c7f0c56bae7fcd1b0cf3732c3cdb
Khalikul <108520856+Khalikul@users.noreply.github.com>
2022-09-12 16:27:08 +0530

    Update flames.py

--- flames.py
+++ flames.py
@@ -1,9 +1,28 @@
-list1 = (input("Enter 1st name: ").lower())
-list2 = input("Enter 2nd name: ").lower()
+name1 = input("Enter 1st name: ").lower()
+name2 = input("Enter 2nd name: ").lower()
 
-for i in range(len(list1)):
-    for j in range(len(list2)):
-        if list1[i] == list2[j]:
-            c = list1[i]
+list1=list(name1)
+list2=list(name2)
 
-            print(c)
+for i in list1[:]:
+    if i in list2:
+        list1.remove(i)
+        list2.remove(i)
+
+count=len(list1)+len(list2)
+
+num=count%6
+if num==1:
+    print("Friends")
+elif num==2:
+    print("love")
+elif num==3:
+    print("Affection")
+elif num==4:
+    print("Marriage")
+elif num==5:
+    print("Enemy")
+elif num==0:
+    print("Siblings")
+
+print(count,num)
